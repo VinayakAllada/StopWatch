514074fcae786c25ea4dfccfdeab7411eca6c3b6
Vinayak Allada <vinayakallada2006@gmail.com>
2025-03-26 13:16:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+// import DigitalClock from './DigitalClock.jsx'
+
+// function App() {
+//   return (<DigitalClock />);
+// }
+
+// export default App
+
+import StopWatch from './StopWatch.jsx'
+
+function App() {
+  return (<StopWatch />);
+}
+
+export default App
